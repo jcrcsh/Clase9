1ea3e7ce0409d9ff1fbff0d22a296f6a4c03012b
José Ruiz <jcrcsh@gmail.com>
2016-06-16 08:21:40 -0500

    sd

--- turtle2.py
+++ turtle2.py
@@ -1,8 +1,7 @@
 import turtle
-n=int(input("ingrese un valor (entero)"))
+n=int(input("ingrese un valor (entero): "))
 t=turtle.Pen()
-
 for x in range(0, n):
     t.forward(100)
     t.left(360/n)
-    turtle.getscreen().root_mainloop()
+    turtle.getscreen()._root_mainloop()
